70b6afc66ad43c1ff0abe57ef2dcb759cf4d8874
nymuelis <daniel.tsv.georgiev@gmail.com>
2023-11-06 23:53:56 +0200

    03-06-11-23

---
+++ React/03.Workshop-Components/client/src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import Header from "./components/Header";
+import "./styles.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
